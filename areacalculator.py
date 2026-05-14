7891794e0d00aeca7d410226e003ea38c3ae2874
Alha890 <as7979@srmist.edu.in>
2026-05-14 11:09:18 +0530

    added files

---
+++ areacalculator.py
@@ -0,0 +1,12 @@
+def area_circle(radius):
+    area = 3.14 * radius * radius
+    print("Area of Circle:", area)
+def area_rectangle(length, width):
+    area = length * width
+    print("Area of Rectangle:", area)
+def area_triangle(base, height):
+    area = 0.5 * base * height
+    print("Area of Triangle:", area)
+area_circle(5)
+area_rectangle(10, 4)
+area_triangle(6, 8)
